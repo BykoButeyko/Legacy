e4a96756d98e6a8f92048bd9bc8e571def8afc60
Byko <bykocrypto@gmail.com>
2021-02-18 21:23:03 +0100

    Legacy smart contract allow multiple successors

--- Legacy.sol
+++ Legacy.sol
@@ -1,18 +1,28 @@
 pragma solidity =0.8.1;
 
 contract Legacy {
-    mapping(address => uint) public checkouts;
+    mapping(address => uint) public checkout;
     mapping(address => uint) public expiration;
+    mapping(address => bool) public rewarded;
+    address public admin;
 
-    function addSuccessor(address kid, uint expiryDate) external payable {
-        checkouts[successor] = msg.value;
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function addSuccessor(address successor, uint expiryDate) external payable {
+        require(msg.sender == admin, 'only admin');
+        require(checkout[msg.sender] == 0, 'successor already added');
+        checkout[successor] = msg.value;
         expiration[successor] = block.timestamp + expiryDate;
     }
     
 
     function withdraw() external {
         require(expiration[msg.sender] <= block.timestamp, 'too early');
-        require(checkouts[msg.sender] > 0, 'only successor can withdraw');
-        payable(msg.sender).transfer(checkouts[msg.sender]);
+        require(checkout[msg.sender] > 0, 'only successor can withdraw');
+        require(rewarded[msg.sender] == false, 'rewarded already');
+        rewarded[msg.sender] = true;
+        payable(msg.sender).transfer(checkout[msg.sender]);
     }
 }
